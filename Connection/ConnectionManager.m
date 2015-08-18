50c4e9729f2f01aa7edf2196ed1f82a637245597
znatz <z.nat.cn@gmail.com>
2015-08-18 15:28:56 +0900

    Format code

--- Connection/ConnectionManager.m
+++ Connection/ConnectionManager.m
@@ -18,50 +18,58 @@
 
 @implementation ConnectionManager
 
+#pragma mark - Get All Database files
+
 + (void) fetchDBFile : (NSString *)dbfileName fromViewController : (UIViewController*) parentView
 {
-    UIView * dimview = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, parentView.view.frame.size.width, parentView.view.frame.size.height)];
-    [dimview setBackgroundColor:[UIColor colorWithWhite:0 alpha:0.5f]];
-    dimview.tag = 10;
+#pragma mark -Local Variables
+    UIView          * dimview;
+    NSString        * dbURI, * url, * documentDirectory, * filePath;
+    NSError         * error;
+    NSArray         * paths;
+    NSFileManager   * fileManager;
+    NSMutableURLRequest           * request;
+    
+    AFHTTPRequestOperationManager * manager;
+    AFHTTPRequestOperation        * operation;
+    
+#pragma mark -Dim Background
+    dimview                 = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, parentView.view.frame.size.width, parentView.view.frame.size.height)];
+    dimview.backgroundColor = [UIColor colorWithWhite:0 alpha:0.5f];
+    dimview.tag             = 10;
     dispatch_async(dispatch_get_main_queue(), ^{
         [[parentView view] addSubview:dimview];
     });
     
     
-    NSString * dbURI = @"http://posco-cloud.sakura.ne.jp/TEST/IOS/OrderSystem/app/database";
-
-    NSString *url    = [NSString stringWithFormat:@"%@/%@", dbURI, dbfileName];
+#pragma mark -Prepare Download Operation
+    dbURI        = @"http://posco-cloud.sakura.ne.jp/TEST/IOS/OrderSystem/app/database";
+    url          = [NSString stringWithFormat:@"%@/%@", dbURI, dbfileName];
     
-    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
-    
-    
-    // ダウンロード先のURLを設定したNSURLRequestインスタンスを生成する
-    NSError * error;
-    NSMutableURLRequest *request = [[manager requestSerializer] requestWithMethod:@"GET" URLString:url parameters:nil error:&error];
-    
-    // ダウンロード処理を実行するためのAFHTTPRequestOperationインスタンスを生成する
-    AFHTTPRequestOperation *operation = [manager HTTPRequestOperationWithRequest:request
-                                                                         success:^(AFHTTPRequestOperation *operation, id responseObject) {
-                                                                             dispatch_async(dispatch_get_main_queue(), ^{
-                                                                                 for (UIView * v in parentView.view.subviews) {
-                                                                                     if (v.tag == 10) {
-                                                                                         [v removeFromSuperview];
-                                                                                     }
-                                                                                 }
-                                                                             });
-                                                                         } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-                                                                             dispatch_async(dispatch_get_main_queue(), ^{
-                                                                                 for (UIView * v in parentView.view.subviews) {
-                                                                                     if (v.tag == 10) {
-                                                                                         [v removeFromSuperview];
-                                                                                     }
-                                                                                 }
-                                                                             });
-                                                                         }];
+    manager      = [AFHTTPRequestOperationManager manager];
+    request      = [[manager requestSerializer] requestWithMethod:@"GET" URLString:url parameters:nil error:&error];
     
     
+    operation    = [manager HTTPRequestOperationWithRequest : request
+                                                    success : ^(AFHTTPRequestOperation *operation, id responseObject) {
+                                                        dispatch_async(dispatch_get_main_queue(), ^{
+                                                            for (UIView * v in parentView.view.subviews)
+                                                            {
+                                                                if (v.tag == 10) [v removeFromSuperview];
+                                                            }
+                                                        });
+                                                    }
+                                                    failure : ^(AFHTTPRequestOperation *operation, NSError *error) {
+                                                        dispatch_async(dispatch_get_main_queue(), ^{
+                                                            for (UIView * v in parentView.view.subviews)
+                                                            {
+                                                                if (v.tag == 10) [v removeFromSuperview];
+                                                            }
+                                                        });
+                                                    }];
     
-    // データを受信する度に実行される処理を設定する
+    
+#pragma mark -Show Progress
     [operation setDownloadProgressBlock:^(NSUInteger bytesRead, long long totalBytesRead, long long totalBytesExpectedToRead) {
         
         dispatch_async(dispatch_get_main_queue(), ^{
@@ -73,14 +81,18 @@
             }
             
             double percentDone = (double)totalBytesRead / (double)totalBytesExpectedToRead ;
-            DALabeledCircularProgressView * progressbar = [[DALabeledCircularProgressView alloc] initWithFrame:CGRectMake([parentView view].frame.size.width/5, [parentView view].frame.size.height/5, [parentView view].frame.size.width*3/5, [parentView view].frame.size.width*3/5)];
+            DALabeledCircularProgressView * progressbar = [[DALabeledCircularProgressView alloc]
+                                                           initWithFrame : CGRectMake([parentView view].frame.size.width/5,
+                                                                                      [parentView view].frame.size.height/5,
+                                                                                      [parentView view].frame.size.width*3/5,
+                                                                                      [parentView view].frame.size.width*3/5)];
             
-            progressbar.roundedCorners = YES;
-            progressbar.trackTintColor = [UIColor brownColor];
-            progressbar.progressTintColor = [UIColor whiteColor];
-            progressbar.progress = percentDone;
-            progressbar.thicknessRatio = 0.1f;
-            progressbar.progressLabel.text = [NSString stringWithFormat:@"%d％", (int)round(percentDone * 100) ];
+            progressbar.progressTintColor               = [UIColor whiteColor];
+            progressbar.trackTintColor                  = [UIColor brownColor];
+            progressbar.roundedCorners                  = YES;
+            progressbar.thicknessRatio                  = 0.1f;
+            progressbar.progress                        = percentDone;
+            progressbar.progressLabel.text              = [NSString stringWithFormat:@"%d％", (int)round(percentDone * 100) ];
 
             if (percentDone < 1) [[parentView view] addSubview:progressbar];
         });
@@ -88,28 +100,19 @@
     
     
     
-    
-    // <Application_Home>/Documentsディレクトリのパスを取得する
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    NSString *documentDirectory = paths[0];
-    
-    // <Application_Home>/Documents/test.zip
-    NSString *filePath = [documentDirectory stringByAppendingPathComponent:dbfileName];
-    
-    NSFileManager *fileManager = [NSFileManager defaultManager];
+    paths                   = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    documentDirectory       = paths[0];
+    filePath                = [documentDirectory stringByAppendingPathComponent:dbfileName];
+    fileManager             = [NSFileManager defaultManager];
     if ([fileManager fileExistsAtPath:filePath]) {
         [fileManager removeItemAtPath:filePath error:nil];
     }
     
-    // ファイルの保存先を「<Application_Home>/Documents/test.zip」に指定する
-    operation.outputStream = [NSOutputStream outputStreamToFileAtPath:filePath append:NO];
-    
-    // ダウンロードを開始する
+    operation.outputStream  = [NSOutputStream outputStreamToFileAtPath:filePath append:NO];
     [manager.operationQueue addOperation:operation];
+    [operation waitUntilFinished];
     
 //    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
-    
-    [operation waitUntilFinished];
 
 }
 
@@ -121,22 +124,23 @@
 
 + (void)uploadFile : (NSString *) dbfileName;
 {
-//    NSString * dbURI = @"http://127.0.0.1:3000/fromIOS";
-//    NSString * dbURI = @"http://posco-cloud.sakura.ne.jp/TEST/iosReceiver.php";
-    NSString * dbURI = @"http://posco-cloud.sakura.ne.jp/TEST/IOS/OrderSystem/public/iosReceiver";
+    NSString * dbURI, * tanto, * documentDir, * uploadLocationPath;
+    NSDictionary * params;
+    NSData * data;
+    AFHTTPRequestOperationManager * manager;
+    
+    tanto               = [[NSUserDefaults standardUserDefaults] objectForKey:@"tantoID"];
+    params              = @{ @"tanto": tanto};
+    
+    dbURI               = @"http://posco-cloud.sakura.ne.jp/TEST/IOS/OrderSystem/public/iosReceiver";
 
-    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    manager             = [AFHTTPRequestOperationManager manager];
     manager.responseSerializer = [AFHTTPResponseSerializer serializer];
     
-    NSString * tanto = [[NSUserDefaults standardUserDefaults] objectForKey:@"tantoID"];
-    NSDictionary *params = @{ @"tanto": tanto};
     
-    NSString *documentDir;
-    documentDir = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents"];
-    NSString *uploadLocationPath = [documentDir stringByAppendingPathComponent:dbfileName];
-    
-    
-    NSData *data = [[NSFileManager defaultManager] contentsAtPath:uploadLocationPath];
+    documentDir         = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents"];
+    uploadLocationPath  = [documentDir stringByAppendingPathComponent:dbfileName];
+    data                = [[NSFileManager defaultManager] contentsAtPath:uploadLocationPath];
 
     [manager POST : dbURI
        parameters : params
@@ -147,7 +151,6 @@ constructingBodyWithBlock : ^( id <AFMultipartFormData> formData )
      }
           success : ^(AFHTTPRequestOperation *operation, id responseObject)
      {
-//         NSLog(@"response is :  %@",responseObject);
      }
           failure:^(AFHTTPRequestOperation *operation, NSError *error)
      {
@@ -160,17 +163,21 @@ constructingBodyWithBlock : ^( id <AFMultipartFormData> formData )
 
 + (void)uploadKey : (NSString *) key value : (NSString *) value
 {
-    NSString * dbURI = @"http://posco-cloud.sakura.ne.jp/TEST/iosReceiver.php";
+    NSString * dbURI;
+    NSDictionary * params;
+    AFHTTPRequestOperationManager * manager;
+    
+    dbURI   = @"http://posco-cloud.sakura.ne.jp/TEST/iosReceiver.php";
 
-    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    manager = [AFHTTPRequestOperationManager manager];
     manager.responseSerializer = [AFHTTPResponseSerializer serializer];
     
-    NSDictionary *params = @{ key : value};
+    params  = @{ key : value};
     
     [manager POST : dbURI
        parameters : params
 constructingBodyWithBlock : ^( id <AFMultipartFormData> formData )
-     {
+    {
      }
           success : ^(AFHTTPRequestOperation *operation, id responseObject)
      {
@@ -179,7 +186,6 @@ constructingBodyWithBlock : ^( id <AFMultipartFormData> formData )
      {
          NSLog(@"Error: %@ *****", [error description]);
      }];
-    
 
 }
 
